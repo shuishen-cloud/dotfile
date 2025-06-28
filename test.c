89a24071405b1d2fb662f0e48433966c7ddc86a2
lwy_coder <201212ss@sina.com>
2025-06-28 18:36:10 +0800

    feat: 添加测试用代码和代码头添加模板
    添加了新的VSCode扩展支持，包括代码翻译、注释增强和更多的git相关工具。具体扩展如下：
    - w88975.code-translate：代码翻译
    - usernamehw.errorlens：错误提示
    - evgeniypeshkov.syntax-highlighter：代码高亮增强
    - aaron-bond.better-comments：注释增强
    - OBKoro1.korofileheader：头部注释模板（延迟较高,定制性较强）
    - mhutchie.git-graph：git图形化界面
    - eamodio.gitlens：git智能提示（部分功能收费）
    
    新增了`.vscode/psi-haeder.json`配置文件，用于设置psioniq.psi-header插件的详细配置，包括作者信息、自动添加头部注释、日期格式等。
    
    修改了`.vscode/settings.json`文件，将终端光标样式设置从默认值更改为块状。
    
    增加了两个测试文件`test.c`和`test.py`，分别包含C语言和Python语言的示例代码，并且每个文件都添加了头部注释模板，详细描述了文件名、项目名、作者、创建时间和修改时间等信息。

---
+++ test.c
@@ -0,0 +1,11 @@
+/*
+ * Filename:  test.c
+ * Project:   dotfile
+ * Author:    lwy
+ * ***
+ * Created:   2025/06/28 Saturday 17:55:25
+ * Modified:  2025/06/28 Saturday 18:32:29
+ * ***
+ * Description: 
+ */
+
